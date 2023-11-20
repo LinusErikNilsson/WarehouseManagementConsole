5e16e3f2c798daf57d47b47ccc4851c095adc090
Linus Nilsson <linus.erik.nilsson@outlook.com>
2023-11-20 13:52:13 +0100

    Created Setup file for SQL Setup queries

---
+++ Setup.sql
@@ -0,0 +1,24 @@
+﻿--- Note Database 'master' is used
+
+-- Create Tables and Constraints
+CREATE TABLE Material(id int IDENTITY PRIMARY KEY NOT NULL, name VARCHAR(50));
+
+CREATE TABLE MaterialStorage (id int IDENTITY PRIMARY KEY NOT NULL, Aisle int, Shelf int, MaterialId int, Quantity int,
+CONSTRAINT FK_Material_MaterialStorage FOREIGN KEY (MaterialId)
+REFERENCES Material(id) );
+
+
+
+--- Insert Data to tables
+--Material
+INSERT INTO Material(name)
+VALUES('Stainless Steel');
+INSERT INTO Material(name)
+VALUES('Iron');
+INSERT INTO Material(name)
+VALUES('Copper');
+
+--MaterialStorage
+INSERT INTO MaterialStorage (Aisle, Shelf, MaterialId, Quantity)
+VALUES(1,1,1,0);
+
