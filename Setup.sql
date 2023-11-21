b2782471a654b516cd167660306d544b49eb9e83
Linus Nilsson <linus.erik.nilsson@outlook.com>
2023-11-21 10:00:32 +0100

    Added all remaining SQL Create queries

--- Setup.sql
+++ Setup.sql
@@ -3,24 +3,58 @@
 -- Create Tables and Constraints
 CREATE TABLE Material(id int IDENTITY PRIMARY KEY NOT NULL, name VARCHAR(50));
 
-CREATE TABLE MaterialStorage (id int IDENTITY PRIMARY KEY NOT NULL, Aisle int, Shelf int, MaterialId int, Quantity int,
-CONSTRAINT FK_Material_MaterialStorage FOREIGN KEY (MaterialId)
-REFERENCES Material(id) );
+CREATE TABLE MaterialStorage (id int IDENTITY PRIMARY KEY NOT NULL, 
+    Aisle int, 
+    Shelf int, 
+    MaterialId int, 
+    Quantity int,
+    CONSTRAINT FK_Material_MaterialStorage FOREIGN KEY (MaterialId)
+    REFERENCES Material(id) );
 
-CREATE TABLE Product(Id int PRIMARY KEY NOT NULL IDENTITY, Name varchar(50));
+CREATE TABLE ProductionQueue (
+    id int IDENTITY PRIMARY KEY NOT NULL,
+	MaterialId int, 
+	Quantity int,
+    Priority int,
+	CONSTRAINT FK_Material_ProductionQueue FOREIGN KEY (MaterialId)
+	REFERENCES Material(id) );
 
-CREATE TABLE ProductOrder(Id int IDENTITY PRIMARY KEY NOT NULL, 
+CREATE TABLE Product(
+    Id int PRIMARY KEY NOT NULL IDENTITY,
+    Name varchar(50));
+
+CREATE TABLE Customer(
+    Id int IDENTITY PRIMARY KEY NOT NULL,
+    Surname varchar(50),
+    LastName varchar(50),
+    Address varchar(50),
+    Email varchar(50),
+    Phonenumber varchar(50));
+
+CREATE TABLE ProductOrder(
+    Id int IDENTITY PRIMARY KEY NOT NULL, 
 	CustomerId varchar(50), 
 	IsPacked bit NOT NULL,
 	IsSent bit NOT NULL,
-	IsDelivered bit NOT NULL);
+	IsDelivered bit NOT NULL,
+    CONSTRAINT FK_Product_ProductOrder FOREIGN KEY (CustomerID)
+    REFERENCES Customer(Id) );
 
-CREATE TABLE ProductStorage(Id int PRIMARY KEY NOT NULL IDENTITY,
-	Aisle VARCHAR(50),
-	Shelf varchar(50),
-	ProductName varchar(50),
-	Quantity int);
+CREATE TABLE ProductStorage (
+    id int IDENTITY PRIMARY KEY NOT NULL, 
+    Aisle int, 
+    Shelf int, 
+    ProductId int, 
+    Quantity int,
+    CONSTRAINT FK_Product_ProductStorage FOREIGN KEY (ProductId)
+    REFERENCES Product(Id) );
 
+   CREATE TABLE ProductQueueForStorage(
+    id int IDENTITY Primary KEY NOT NULL,
+    ProductId int,
+    Quantity int,
+    CONSTRAINT FK_Product_ProductQueueForStorage FOREIGN KEY (ProductId)
+    REFERENCES Product(Id) );
 
 
 -- Kopplingstabell MaterialToProduct
