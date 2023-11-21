b5e7e2e42d7f1ff2722bec8c32c9adb3dddd918f
Linus Nilsson <linus.erik.nilsson@outlook.com>
2023-11-21 09:09:07 +0100

    Added more SQL to create tables

--- Setup.sql
+++ Setup.sql
@@ -7,6 +7,42 @@ CREATE TABLE MaterialStorage (id int IDENTITY PRIMARY KEY NOT NULL, Aisle int, S
 CONSTRAINT FK_Material_MaterialStorage FOREIGN KEY (MaterialId)
 REFERENCES Material(id) );
 
+CREATE TABLE Product(Id int PRIMARY KEY NOT NULL IDENTITY, Name varchar(50));
+
+CREATE TABLE ProductOrder(Id int IDENTITY PRIMARY KEY NOT NULL, 
+	CustomerId varchar(50), 
+	IsPacked bit NOT NULL,
+	IsSent bit NOT NULL,
+	IsDelivered bit NOT NULL);
+
+CREATE TABLE ProductStorage(Id int PRIMARY KEY NOT NULL IDENTITY,
+	Aisle VARCHAR(50),
+	Shelf varchar(50),
+	ProductName varchar(50),
+	Quantity int);
+
+
+
+-- Kopplingstabell MaterialToProduct
+CREATE TABLE MaterialToProduct (
+    ProductID int,
+     MaterialID int,
+     PRIMARY KEY (ProductId, MaterialID),
+     FOREIGN KEY (ProductId) REFERENCES Product(Id),
+     FOREIGN KEY (MaterialId) REFERENCES Material(Id));
+
+---Kopplingstabell ProductToOrder
+CREATE TABLE ProductToOrder(
+    ProductId int,
+    OrderId int,
+    PRIMARY KEY (ProductId, OrderId),
+    FOREIGN KEY (ProductId) REFERENCES Product(Id),
+    FOREIGN KEY (OrderId) REFERENCES ProductOrder(Id));
+
+
+
+
+
 
 
 --- Insert Data to tables
@@ -21,4 +57,8 @@ VALUES('Copper');
 --MaterialStorage
 INSERT INTO MaterialStorage (Aisle, Shelf, MaterialId, Quantity)
 VALUES(1,1,1,0);
+
+--Product
+INSERT INTO Product(Name)
+VALUES('Stainless Nut');
 
